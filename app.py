325e3d19414fd274f1fcf89d7f64402cb1f14382
David Santiago <vergarita29lleme@gmail.com>
2026-05-01 00:07:54 -0500

    Conexion Flask con MYSQUL configurada más Conexion Flask y Frontend establesida

--- app.py
+++ app.py
@@ -1,10 +1,27 @@
 from flask import Flask, render_template
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
+
+# Configuracion de la base de datos
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'DavidSantiago2005**'
+app.config['MYSQL_DB'] = 'asisttrack'
+
+mysql = MySQL(app)
 
 @app.route('/')
 def inicio():
     return render_template('login.html')
+
+@app.route('/empleados')
+def empleados():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM empleados")
+    empleados = cur.fetchall()
+    cur.close()
+    return render_template('empleados.html', empleados=empleados)
 
 if __name__ == '__main__':
     app.run(debug=True)
