622c7519e314eb4e4f31f4d333bb6427e38cef67
David Santiago <vergarita29lleme@gmail.com>
2026-05-04 15:51:56 -0500

    Pantalla de asistencia diaria funcionando

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, redirect
 from flask_mysqldb import MySQL
+from datetime import date
 
 app = Flask(__name__)
 
@@ -39,6 +40,26 @@ def agregar_empleado():
         cur.close()
         return redirect('/empleados')
     return render_template('agregar_empleado.html')
+
+@app.route('/asistencia', methods=['GET', 'POST'])
+def asistencia():
+    cur = mysql.connection.cursor()
+    cur.execute("SELECT * FROM empleados WHERE activo = TRUE")
+    empleados = cur.fetchall()
+    cur.close()
+    
+    if request.method == 'POST':
+        fecha_hoy = date.today()
+        cur = mysql.connection.cursor()
+        for empleado in empleados:
+            estado = request.form.get(f'asistencia_{empleado[0]}')
+            cur.execute("INSERT INTO asistencia (empleado_id, fecha, estado) VALUES (%s, %s, %s)",
+                       (empleado[0], fecha_hoy, estado))
+        mysql.connection.commit()
+        cur.close()
+        return redirect('/asistencia')
+    
+    return render_template('asistencia.html', empleados=empleados, fecha=date.today())
 
 if __name__ == '__main__':
     app.run(debug=True)
