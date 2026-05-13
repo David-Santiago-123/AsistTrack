43b9bd624746699c5c5ba5848f005c31e631681f
David Santiago <vergarita29lleme@gmail.com>
2026-05-12 22:56:53 -0500

    Variables de entorno y Seguridad Implementada

--- app.py
+++ app.py
@@ -4,15 +4,19 @@ from datetime import date
 import openpyxl
 from io import BytesIO
 from flask import send_file
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print(os.getenv('MYSQL_PASSWORD'))
 
 app = Flask(__name__)
-app.secret_key = 'asisttrack2026'
+app.secret_key = os.getenv('SECRET_KEY')
 
-# Configuracion de la base de datos
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'DavidSantiago2005**'
-app.config['MYSQL_DB'] = 'asisttrack'
+app.config['MYSQL_HOST'] = os.getenv('MYSQL_HOST')
+app.config['MYSQL_USER'] = os.getenv('MYSQL_USER')
+app.config['MYSQL_PASSWORD'] = os.getenv('MYSQL_PASSWORD')
+app.config['MYSQL_DB'] = os.getenv('MYSQL_DB')
 
 mysql = MySQL(app)
 
